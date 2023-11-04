b3f9c69651270a6afb385ef84b56492c973ea850
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-11-04 14:25:59 +0200

    Creational Patterns Updates 1

--- Design Patterns.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Design Patterns.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,8 +1,11 @@
 import Foundation
 
-//Creational Design Patterns
+// MARK: - Creational Design Patterns
 
 // MARK: - 1. Singleton
+
+//ده باترن بيخليني أعمل اوبجكت واحد بس من الكلاس ويكون عام علي مستوي التطبيق
+//تستخدمه من أي كلاس تاني عالطول من غير متعمل كل شويه اوبجكت جديد
 
 //Implementation
 class Singleton {
@@ -16,9 +19,12 @@ class Singleton {
 
 //Using
 
-//Singleton.shared.printingFunction()
+Singleton.shared.printingFunction()
 
 // MARK: - 2. Prototype
+
+//الباترن ده بيسمحلك انك تعمل ابجكت جديد عن طريق انك تعمل نسخ للأبجكت الموجود وتبدأ تغير في بعض الأتربيوت
+// بدل متعمل كل شويه ابجكت جديد في اتربيوت كتير متشابهه
 
 //Implementation
 class MoonWorker {
@@ -36,14 +42,134 @@ class MoonWorker {
 }
 //Using
 
-//let prototype = MoonWorker(name: "Sam Bell")
-//let proto1 = prototype.clone()
-//proto1
-//let proto2 = prototype.clone()
-//proto2.name = "Ahmed"
-//proto2
+let prototype = MoonWorker(name: "Sam Bell")
+let proto1 = prototype.clone()
+proto1
+let proto2 = prototype.clone()
+proto2.name = "Ahmed"
+proto2
 
 // MARK: - 3. Builder
+
+// Build complex object step by step
+//هي أنك بتبني الاوبجكت بتاعك جزي جزء وفي الأخر تستخدم ميثود تجمع كل الاجزاء مع بعض وتكونلك الاوبجكت بتاعك
+
+// Example 1: Building car
+
+struct Product{
+    
+    var parts: [String] = []
+    
+    mutating func addPart(part: String){
+        parts.append(part)
+    }
+    func show(){
+        var allParts: String = ""
+        for part in parts{
+            allParts += part + "\n"
+        }
+        print(allParts)
+    }
+}
+
+protocol Builder{
+    func startOperations()
+    func buildBody()
+    func insertWheels()
+    func addHeadLights()
+    func endOperations()
+    func getVehicle() -> Product
+}
+
+//ConcreteBuilder Class
+class Car: Builder{
+    private var brandName: String
+    private var product: Product
+    
+    init(brandName: String) {
+        product = Product()
+        self.brandName = brandName
+    }
+    
+    func startOperations() {
+        product.addPart(part: "Car Model name is \(brandName)")
+    }
+    func buildBody() {
+        product.addPart(part: "Body of car is added")
+    }
+    func insertWheels() {
+        product.addPart(part: "Wheels are added")
+    }
+    func addHeadLights() {
+        product.addPart(part: "HeadLights are added")
+    }
+    func endOperations() {
+//        print("End Operation")
+    }
+    func getVehicle() -> Product {
+        return product
+    }
+}
+class MotorCycle: Builder{
+    private var brandName: String
+    private var product: Product
+    
+    init(brandName: String) {
+        product = Product()
+        self.brandName = brandName
+    }
+    
+    func startOperations() {
+        product.addPart(part: "MotorCycle Model name is \(brandName)")
+    }
+    func buildBody() {
+        product.addPart(part: "Body of MotorCycle is added")
+    }
+    func insertWheels() {
+        product.addPart(part: "Wheels are added")
+    }
+    func addHeadLights() {
+        product.addPart(part: "HeadLights are added")
+    }
+    func endOperations() {
+//        print("End Operation")
+    }
+    func getVehicle() -> Product {
+        return product
+    }
+}
+
+class Director{
+    var builder: Builder?
+    func constuct(builder: Builder){
+        self.builder = builder
+        builder.startOperations()
+        builder.buildBody()
+        builder.insertWheels()
+        builder.addHeadLights()
+        builder.endOperations()
+    }
+}
+
+//Usage
+
+let carBuilder: Builder = Car(brandName: "Jeep")
+let motorCycleBuilder: Builder = MotorCycle(brandName: "Honda")
+
+let director = Director()
+
+//Making Car
+director.constuct(builder: carBuilder)
+let car: Product = carBuilder.getVehicle()
+car.show()
+
+//Making MotorCycle
+director.constuct(builder: motorCycleBuilder)
+let motorCycle: Product = motorCycleBuilder.getVehicle()
+motorCycle.show()
+
+
+// Example 2:
 
 //Implementation
 final class DeathStarBuilder {
@@ -80,16 +206,23 @@ struct DeathStar : CustomStringConvertible {
 
 //Usage
 
-//let empire = DeathStarBuilder { builder in
-//    builder.x = 0.1
-//    builder.y = 0.2
-//    builder.z = 0.3
-//}
-//let deathStar = DeathStar(builder:empire)
-//deathStar?.description
+let empire = DeathStarBuilder { builder in
+    builder.x = 0.1
+    builder.y = 0.2
+    builder.z = 0.3
+}
+let deathStar = DeathStar(builder:empire)
+deathStar?.description
 
 // MARK: - 4. Factory Method
 
+// Create object without exposing the creation logic to the client and
+// refer to created object using  a common interface
+// هو باترن بيخلي فيه حاجه زي كلاس مثلا يكون هو اللي مسئول عن انشاء ال ابجيكت
+// بحيث لو عايز اضيف او اعدل في اوبجيكت اكون عارف المكان ال هضيف فيه
+
+
+// EXample 1:
 //Implementation
 protocol CurrencyDescribing {
     var symbol: String { get }
@@ -137,14 +270,94 @@ enum CurrencyFactory {
 //Usage
 
 //let noCurrencyCode = "No Currency Code Available"
-//
 //CurrencyFactory.currency(for: .greece)?.code ?? noCurrencyCode
 //CurrencyFactory.currency(for: .spain)?.code ?? noCurrencyCode
 //CurrencyFactory.currency(for: .unitedStates)?.code ?? noCurrencyCode
 //CurrencyFactory.currency(for: .uk)?.code ?? noCurrencyCode
 
+// Example 2: Bank - ATM
+
+protocol Bank{
+    func withdrow() -> String
+}
+class BankA: Bank{
+    func withdrow() -> String{
+        return "Your request is handling by BankA"
+    }
+}
+class BankB: Bank{
+    func withdrow() -> String{
+        return "Your request is handling by BankB"
+    }
+}
+
+protocol BankService{
+    func getBank(bankCode: String) -> Bank?
+    func getPaymentCard(cardNumber: String) -> PaymentCard?
+}
+class BankFactory: BankService{
+    func getBank(bankCode: String) -> Bank? {
+        switch bankCode{
+        case "123":
+            return BankA()
+        case "111":
+            return BankB()
+        default:
+            return nil
+        }
+    }
+    func getPaymentCard(cardNumber: String) -> PaymentCard? {
+        switch cardNumber{
+        case "123":
+            return VisaCard()
+        case "111":
+            return MasterCard()
+        default:
+            return nil
+        }
+    }
+}
+
+let bankFactory = BankFactory()
+let bank: Bank? = bankFactory.getBank(bankCode: "111")
+bank?.withdrow()
+
+
 // MARK: - 5. Abstract Factory
 
+
+
+//Example 1: Bank - ATM .. Continue..
+protocol PaymentCard{
+    func getName() -> String
+    func getProviderInfo() -> String
+}
+
+class VisaCard: PaymentCard{
+    func getName() -> String {
+        return "Visa Card"
+    }
+    
+    func getProviderInfo() -> String {
+        return "Visa"
+    }
+}
+class MasterCard: PaymentCard{
+    func getName() -> String {
+        return "Master Card"
+    }
+    
+    func getProviderInfo() -> String {
+        return "Master"
+    }
+}
+
+let paymentCard: PaymentCard? = bankFactory.getPaymentCard(cardNumber: "123")
+paymentCard?.getName()
+
+
+
+//Example 2:
 //Implementation
 protocol BurgerDescribing {
     var ingredients: [String] { get }
@@ -198,9 +411,9 @@ enum BurgerFactoryType: BurgerMaking {
 
 //Usage
 
-BurgerFactoryType.bigKahuna.make()
-BurgerFactoryType.jackInTheBox.make()
-BurgerFactoryType.KFC_Burger.make()
+//BurgerFactoryType.bigKahuna.make()
+//BurgerFactoryType.jackInTheBox.make()
+//BurgerFactoryType.KFC_Burger.make()
 
 // MARK: - Structural Design Patterns
 
